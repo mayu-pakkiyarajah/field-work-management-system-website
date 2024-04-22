d8f7d0a02f433d412b23f757af3230f4e2161923
SugeevanY <acyogathassugeevan@gmail.com>
2024-04-22 21:43:41 +0530

    Login & Signup created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import {AuthLayoutComponent} from "./auth-layout/auth-layout.component";
+import {LoginComponent} from "./login/login.component";
+import {SignupComponent} from "./signup/signup.component";
+import {LandingComponent} from "./landing/landing.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+  path:'auth',
+  component: AuthLayoutComponent,
+  children: [
+    {path:'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent},
+    // {path: 'OTP-Verification', component:OTPVerificationComponent}
+  ]
+},
+  {path:'', component:LandingComponent, pathMatch:'full'},
+  {path:'**',component:LoginComponent}
+];
